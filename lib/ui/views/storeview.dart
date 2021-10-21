09a88e3ccda3cfaa2e4b945df33b933737926bac
EluroNneka <68165189+EluroNneka@users.noreply.github.com>
2021-10-21 19:52:38 +0100

    More fix

--- lib/ui/views/storeview.dart
+++ lib/ui/views/storeview.dart
@@ -10,7 +10,6 @@ import 'package:drohometask/ui/views/productview.dart';
 import 'package:drohometask/ui/widget/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'cartview.dart';
 
